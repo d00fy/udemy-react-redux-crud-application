6bd8e0f4a46618829777c364f0ba2290b3ffdb57
d00fy <doofy2020@icloud.com>
2019-12-04 15:02:26 +0900

    Learn state and setState

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 function App() {
-  const profiles = [
-    { name: "Taro", age: 10 },
-    { name: "Hanako", age: 5 },
-    { name: "NoName" }
-  ]
-  return (
-    <div>
-      {
-        profiles.map((profile, index) => {
-          return <User name={profile.name} age={profile.age} key={index} />
-        })
-      }
-    </div>
-  );
+  return (<Counter></Counter>);
 }
 
-const User = (props) => {
-  return <div>Hi I am {props.name} and {props.age} years old </div>
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    console.log(this.state);
+
+    this.state = { count: 0 }
+  }
+
+  handlePlusButton() {
+    console.log("clicked");
+    console.log(this.state.count);
+
+    this.setState({ count: this.state.count + 1 })
+  }
+
+  handleMinusButton() {
+    console.log("clicked");
+    console.log(this.state.count);
+
+    this.setState({ count: this.state.count - 1 })
+  }
+
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>counter: {this.state.count}</div>
+        <button onClick={() => { this.handlePlusButton() }}>＋1</button>
+        {/*このクラスのインスタンスのメソッドをコールして、そのメソッドの中でstateの値を＋1したいので、this.関数*/}
+        <button onClick={() => { this.handleMinusButton() }}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
 
-User.defaultProps = {
-  age: 1
-}
+
 
 export default App;
